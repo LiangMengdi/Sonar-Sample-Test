6b96f8f6b065eeee6bd6537572972f68705edeb7
LiangMengdi <46918376+LiangMengdi@users.noreply.github.com>
2019-09-09 16:20:15 +0800

    Update AssemblyInfo.cs

--- ConsoleApplicationTest/Properties/AssemblyInfo.cs
+++ ConsoleApplicationTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SomeConsoleApplicationTest")]
+[assembly: AssemblyTitle("ConsoleApplicationTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SomeConsoleApplicationTest")]
+[assembly: AssemblyProduct("ConsoleApplicationTest")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
